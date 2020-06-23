8c7de47222fa5e018ff7304f91ccb361e9569766
Ruslan <someonefromua@gmail.com>
2020-06-23 20:23:26 +0300

    v0.99

--- src/components/MoviesList/collapse.js
+++ src/components/MoviesList/collapse.js
@@ -17,15 +17,16 @@ class CollapseComponent extends Component {
       return (
           <div className={flag? 'p-3 d-flex flex-wrap': 'groupList'}>
               {genres.map(value =>
-                  <ListGroupItem  key={value.id} className={` ${flag && 'w-50'} ${darkTheme.isDarkTheme? "DarkTheme": "BrigtTheme"} ${+curGenre === value.id? 'bg-primary': ''}`}>
-                  <NavLink onClick={func}  to={`/page/1?genre=${value.id}`}><span className={darkTheme.isDarkTheme? 'text-white': +curGenre === value.id? 'text-white': 'text-primary' }>{value.name}</span></NavLink>
+                  <ListGroupItem  key={value.id} className={` ${flag && 'w-50'} ${darkTheme.isDarkTheme? "DarkTheme": "BrigtTheme"} ${+curGenre === value.id && 'bg-primary'}`}>
+                  <NavLink onClick={func}  to={`/page/1?genre=${value.id}`}>
+                      <span className={darkTheme.isDarkTheme? 'text-white': +curGenre === value.id? 'text-white': 'text-primary' }>{value.name}</span>
+                  </NavLink>
                   </ListGroupItem>
               )}
           </div>
       )
   }
 }
-
 const masStateToProps = (store)=>{
     const {mainReducer: {curGenre, genres}} = store;
     return{
