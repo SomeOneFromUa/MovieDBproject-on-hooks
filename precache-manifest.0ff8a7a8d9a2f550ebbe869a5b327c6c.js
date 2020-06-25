14b751aa16502620de381d3bae94b7094632f62a
Ruslan <someonefromua@gmail.com>
2020-06-25 17:22:52 +0300

    Updates

---
+++ precache-manifest.0ff8a7a8d9a2f550ebbe869a5b327c6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4a631d64fcec4ef22285b42d885a25",
+    "url": "/MovieDBproject-on-hooks/index.html"
+  },
+  {
+    "revision": "a80510cbc86ad18977c6",
+    "url": "/MovieDBproject-on-hooks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e1baeefb34d4b4a11ae8",
+    "url": "/MovieDBproject-on-hooks/static/css/main.3a27de73.chunk.css"
+  },
+  {
+    "revision": "a80510cbc86ad18977c6",
+    "url": "/MovieDBproject-on-hooks/static/js/2.f03aa32e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MovieDBproject-on-hooks/static/js/2.f03aa32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1baeefb34d4b4a11ae8",
+    "url": "/MovieDBproject-on-hooks/static/js/main.6c2eea21.chunk.js"
+  },
+  {
+    "revision": "6829350ee0aa7f855fde",
+    "url": "/MovieDBproject-on-hooks/static/js/runtime-main.d3f91edb.js"
+  },
+  {
+    "revision": "ac0ac95d37b2d1ea75e528dfeb437a87",
+    "url": "/MovieDBproject-on-hooks/static/media/logo.ac0ac95d.png"
+  }
+]);
